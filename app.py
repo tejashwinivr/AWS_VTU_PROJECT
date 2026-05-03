364a131df12c1b3803b808f1209e6e1ea0ac9b1a
tejashwinivr <167219541+tejashwinivr@users.noreply.github.com>
2026-05-03 10:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 s3 = boto3.client(
     's3',
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),,
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
     region_name='eu-north-1',
     config=Config(signature_version='s3v4')
 )
